1df14812e0db643c822d2383067ba25f1e0e5f90
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2022-12-18 18:06:10 +0700

    Finish Props

--- src/components/FrameworkPop.vue
+++ src/components/FrameworkPop.vue
@@ -1,10 +1,20 @@
 <script lang="ts" setup>
 import { ref } from "vue";
-const props = defineProps<{
-  name: string;
-  img: string;
-  rating: number;
-}>();
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  img: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+});
 const count = ref(props.rating);
 function inc() {
   count.value++;
